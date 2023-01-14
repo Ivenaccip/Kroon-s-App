d6c7d40cedfb3e73d65e809abae44293fe1c0756
ivenaccip <A01653722@itesm.mx>
2023-01-14 00:16:19 -0600

    v 0.0.3

---
+++ diccionario_hashtags.py
@@ -0,0 +1,12 @@
+hashtags = {
+    'villa_massa':["#VillaMassa", "#Amaretto"],
+    'Pindakaaswinkel':["#DePindakaaswinkel", "#appeltaart", "#pindakaas"],
+    'Jos_Poell':["#JosPoell", "#Gebakbodem", "#altijdietstevieren"],
+    'Jules_Destrooper':["#julesdestrooper", "#buttercrisp", "#datisechtanderekoek"],
+    'Verstegen':["#verstegenfriends", "#hetalternatiefvoorzout", "#goudishetnieuwezout"],
+    'Leksands':["#leksands", "#leksandsknäckebröd", "#knäckebröd"],
+    'The_Glenlivet':["#theglenlivet", "#theglenlivetfoundersreserve", "#originalbytradition"],
+    'ANTA_flu':["#antaflu", "#keelpastilles", "#frisseadem"],
+    'Blue_Band':["#samenmetblueband", "#roombeter", "#plantaardig"],
+    'Good4U':["#superseed", "#saladtopper", "#feelthebenefit"]
+    }
--- stack.py
+++ stack.py
@@ -1,24 +1,15 @@
-from tkinter import *
-
-top = Tk()
-width = top.winfo_screenwidth()
-height = top.winfo_screenheight()
-top.geometry("%dx%d"  %(width, height))
-distance = width/2
-L1 = Label(top, text = "Physics")
-L1.place(x = distance + 10,y = 10)
-E1 = Entry(top, bd = 5, width=50)
-E1.place(x = distance +60,y = 10)
-L2 = Label(top,text = "Maths")
-L2.place(x = distance +10,y = 50)
-E2 = Entry(top,bd = 5)
-E2.place(x = distance +60,y = 50)
-
-L3 = Label(top,text = "Total")
-L3.place(x = distance +10,y = 150)
-E3 = Entry(top,bd = 5)
-E3.place(x = distance +60,y = 150)
-
-B = Button(top, text = "Add")
-B.place(x = distance +100, y = 100)
-top.mainloop()
+from ttkwidgets import CheckboxTreeview
+import tkinter as tk
+ 
+root = tk.Tk()
+ 
+tree = CheckboxTreeview(root)
+tree.pack()
+ 
+tree.insert("", "end", "1", text="1")
+tree.insert("1", "end", "11", text="11")
+tree.insert("1", "end", "12",  text="12")
+tree.insert("11", "end", "111", text="111")
+tree.insert("", "end", "2", text="2")
+ 
+root.mainloop()
